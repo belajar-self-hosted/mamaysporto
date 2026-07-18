64b3f9df7eb1421f8a5c9201cd61f917d5e72e49
Fannandya Sutan <sfannandya@gmail.com>
2026-07-18 07:58:59 +0700

    menambahkan project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,19 +2,41 @@ export const projects = [
   {
     id: 1,
     title: "Aplikasi Desktop Manajemen Sarpras",
-    description: "Aplikasi desktop yang digunakan untuk sistem manajemen sarana dan prasarana di prodi Teknik Informasi Universitas Muhammadiyah Yogyakarta (UMY).",
+    description:
+      "Aplikasi desktop yang digunakan untuk sistem manajemen sarana dan prasarana di prodi Teknik Informasi Universitas Muhammadiyah Yogyakarta (UMY).",
     image: "https://placehold.co/600x400/0b0c10/ffffff?text=Sarpras+App",
     link: "#",
     github: "#",
-    tags: ["Desktop App", "Management", "UMY"]
+    tags: ["Desktop App", "Management", "UMY"],
   },
   {
     id: 2,
     title: "Website Portofolio Pribadi",
-    description: "Website portofolio pribadi sebelumnya yang pernah di-deploy di alyanadine.my.id (status saat ini sudah discontinue).",
+    description:
+      "Website portofolio pribadi sebelumnya yang pernah di-deploy di alyanadine.my.id (status saat ini sudah discontinue).",
     image: "https://placehold.co/600x400/ff5470/0b0c10?text=Portfolio",
     link: "https://alyanadine.my.id",
     github: "#",
-    tags: ["Web", "Portfolio"]
-  }
+    tags: ["Web", "Portfolio"],
+  },
+  {
+    id: 3,
+    title: "Website Chatbot AI untuk Portfolio",
+    description:
+      "Website chatbot AI yang digunakan untuk memberikan informasi tentang biodata dan profil Fannandya Sutan Sakti Pratama (Tama) secara interaktif.",
+    image: "https://placehold.co/600x400/ff5470/0b0c10?text=Consume+API",
+    link: "https://aspriguatuh.my.id",
+    github: "#",
+    tags: ["Web", "Portfolio"],
+  },
+  {
+    id: 4,
+    title: "Home Server ",
+    description:
+      "Home server pribadi yang saya kembangkan sendiri untuk keperluan belajar dan eksperimen di bidangg DevOps, Networking, dan Web Development. Server ini saya gunakan untuk hosting beberapa aplikasi web pribadi (seperti web ini) dan eksperimen dengan berbagai teknologi menggunakan linux.",
+    image: "https://placehold.co/600x400/ff5470/0b0c10?text=Home+Server",
+    link: "",
+    github: "#",
+    tags: ["Hosting", "DevOps", "Linux"],
+  },
 ];
